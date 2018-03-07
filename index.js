27ac48e14620cbff6d8b0b732a66b7e9f40163eb
Damien Gilles <pro@gillesdamien.com>
2018-03-07 15:09:25 +0100

    index.js ongoing

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 
 function init() {
-    const plante1 =new Plant(120,50,50,'sunny',true);
 
-    console.log(plante1);
+    const UI = new UI(
+        document.querySelector("#actionBar > .water"),
+        document.querySelector("#actionBar > .food"),
+        document.querySelector("#actionBar > .light"), 
+        document.querySelector("#location"));
 
-    const plante2= new Plant(50,50,50,'sunny',true);
-    console.log(plante2);
-    for(let i=0;i<10;i++){
-        plante2.nextStep();
-        console.log(plante2);
-    }
-    
+    const plant = new Plant(120,50,50,'sunny',true);
+
+    const WeatherAPI()
 }
