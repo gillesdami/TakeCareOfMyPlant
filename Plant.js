7b43a1dfa0088a367369fe213dca911a94f41db5
lecontemat <lecontemat@eisti.eu>
2018-03-07 14:48:00 +0100

    edit deathOrigin

--- Plant.js
+++ Plant.js
@@ -1,5 +1,14 @@
+const NOTDEAD=0;
+const FLOOD=1;
+const NOWATER=2;
+const FIRE=3;
+const NOSUN=4;
+const FAT=5;
+const HUNGER=6;
+const OTHER=7;
 
 class Plant {
+    
     constructor(water, sun, nutrients, weather,isUVLamp){
         this.isDead=false;
         this.setWater(water);
@@ -112,21 +121,21 @@ class Plant {
 
     deathOrigin(){
         if(!this.isDead){
-            return "plant not dead"
+            return NOTDEAD
         }else if(this.water==100){
-            return "flood"
+            return FLOOD
         }else if(this.sun==100){
-            return "fire"
+            return FIRE
         }else if(this.nutiment==100){
-            return "fat"
+            return FAT
         }else if(this.water==0){
-            return "no water"
+            return NOWATER
         }else if(this.sun==0){
-            return "no light"
+            return NOSUN
         }else if(this.nutiment==0){
-            return "no nutriment"
+            return HUNGER
         }else{
-            return "bad tratement"
+            return OTHER
         }
     }
 
--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 
 function init() {
-    const plante1 =new Plant(120,50,50,'sunny',true);
 
-    console.log(plante1);
-
-    const plante2= new Plant(50,50,50,'sunny',true);
-    console.log(plante2);
-    for(let i=0;i<10;i++){
-        plante2.nextStep();
-        console.log(plante2);
-    }
     
 }
